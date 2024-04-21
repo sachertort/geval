db72ebe3b70b7fa797bbecabbf37cf60af956cb5
sachertort <roman-k2000@mail.ru>
2024-04-21 14:30:57 +0400

    Evaluate 100 samples

--- geval_freud.py
+++ geval_freud.py
@@ -30,7 +30,7 @@ def query(cur_prompt: str, model: str, client: openai.OpenAI) -> float:
                                                    stop=None,
                                                    logprobs=False,
                                                    n=20)
-        time.sleep(0.2)
+        time.sleep(0.3)
         all_responses = [_response.choices[i].message.content
                          for i in range(len(_response.choices))]
         all_scores = []
@@ -86,11 +86,10 @@ if __name__ == "__main__":
             prompt = f.read()
 
         all_scores = []
-        for instance in tqdm(data[:20]):
+        for instance in tqdm(data[:100]):
             dialog = f"""USER: {instance['utt_1']}\nBOT: {instance['utt_2']}\n
                          USER: {instance['utt_3']}\nBOT: {instance['generated_response']}"""
             cur_prompt = prompt.replace("{{Dialog}}", dialog)
-            # instance["prompt"] = cur_prompt
             while True:
                 try:
                     final_score = query(cur_prompt, args.model, client)
